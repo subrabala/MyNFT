9302afd75cdc9029c411d43c71f8c2383490a742
subrabala <89636755+subrabala@users.noreply.github.com>
2022-11-15 10:11:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import "./App.css";
 import FirstPage from "./components/FirstPage";
 
 function App() {
+  var marker = document.querySelector('#marker');
+                    function indicator(e){
+                    marker.style.left = e.offsetLeft+"px";
+                    marker.style.width = e.offsetWidth+"px";
+                    }
+
+                    items.forEach((item)=>{
+                    item.addEventListener('click',(e)=>{
+                        indicator(e.target);
+                    })
+                    })
   return (
     <div className="container p-0 m-0">
       <FirstPage />
