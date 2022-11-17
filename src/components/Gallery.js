2c4b727ec5eee42151d0dd8ba8ee41737e88c826
Subrabala Dash <subrabala_dash@srmap.edu.in>
2022-11-18 01:20:18 +0530

    cardspage

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,13 +1,66 @@
 import React from "react";
+import CardComp from "./CardComp";
 
+const data = [
+  {
+    id: 1,
+    title: "monkey",
+    desc: "a bitcoin with bla bla",
+    subtitle: "leedong wwook",
+  },
+  {
+    id: 2,
+    title: "rakooon",
+    desc: "an ether with bla bla",
+    subtitle: "doraemon nobitahahahhaaaa",
+  },
+  {
+    id: 3,
+    title: "bandar",
+    desc: "a phooti kaudi with bla bla",
+    subtitle: "tereku kyu",
+  },
+  {
+    id: 4,
+    title: "banddsfdar",
+    desc: "a phoosdvti kaudi with bla bla",
+    subtitle: "tereku kyu",
+  },
+  {
+    id: 11,
+    title: "monkey",
+    desc: "a bitcoin with bla bla",
+    subtitle: "leedong wwook",
+  },
+  {
+    id: 22,
+    title: "rakooon",
+    desc: "an ether with bla bla",
+    subtitle: "doraemon nobitahahahhaaaa",
+  },
+  {
+    id: 43,
+    title: "bandar",
+    desc: "a phooti kaudi with bla bla",
+    subtitle: "tereku kyu",
+  },
+  {
+    id: 44,
+    title: "banddsfdar",
+    desc: "a phoosdvti kaudi with bla bla",
+    subtitle: "tereku kyu",
+  },
+];
 
-const Gallery=()=>{
-    return (
-        <div className="container p-0 m-0">
-        <cardComp />
-      </div>
-    )
+const Gallery = () => {
+  return (
+    <div className="container-fluid m-0" style={{padding:'45px'}} >
+       <div className="row">
+       { data.map(record=><CardComp coin={record} key={record.id}/>)
+        }
+       </div>
+    </div>
+  );
+};
 
-}
-
-export default Gallery
+export default Gallery;
--- src/components/cardComp.js
+++ src/components/cardComp.js
@@ -1,32 +1,22 @@
 import React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-export default function cardComp() {
-    return (
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          alt="green iguana"
-          height="140"
-          image="assets/Rectangle 2.png"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Monkemoney18
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-           
-          </Typography>AP21110010678
-        </CardContent>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
-    );
-  }
+const CardComp=({coin})=> {
+  return (
+    <div className='col-3 p-2'>
+        <Card style={{ width: 'auto' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>{coin.title}</Card.Title>
+        <Card.Text>
+          {coin.desc}
+        </Card.Text>
+        <Button variant="primary">{coin.subtitle}</Button>
+      </Card.Body>
+    </Card>
+    </div>
+  );
+}
+
+export default CardComp;
